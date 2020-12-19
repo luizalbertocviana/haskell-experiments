63d8edec246f80f3d98182dad3d1e2f8f3b58b91
Luiz Alberto <luizalbertocviana@gmail.com>
2020-12-19 10:34:19 -0300

    major modifications in Matrix.hs

--- Matrix.hs
+++ Matrix.hs
@@ -3,13 +3,16 @@
 
 module Matrix where
 
-import Prelude ((*), (+), repeat, zip, (<$>), map, max, (.), Num, Functor(fmap), Applicative(pure, (<*>)), Show(show), error)
+import Prelude (($), (*), (+), repeat, zip, (<$>), map, max, (.), Num, Functor(fmap), Applicative(pure, (<*>)), Show(show), error)
 import Data.Eq (Eq((==)))
 import Data.List (intercalate)
+import Data.List (foldl')
 import qualified Data.List as List
 import Data.Foldable (Foldable(sum))
 import Data.Array (listArray, (!), Array)
 import Data.Word (Word)
+import Data.Array.Unsafe (unsafeThaw)
+import Data.Array.ST (writeArray, runSTArray)
 
 type RectIdx = (Word, Word)
 
@@ -47,12 +50,16 @@ toFn :: Matrix Arr a -> Matrix Fn a
 toFn (M dim (Arr arr)) = M dim (Fn f) where
   f pos = arr ! pos
 
+toArr :: Matrix Fn a -> Matrix Arr a
+toArr (M dim@(r, c) (Fn f)) = M dim (Arr arr) where
+  arr = listArray ((1, 1), dim) (map f ((,) <$> [1..r] <*> [1..c]))
+
 instance (Show a, MatrixRep r) => Show (Matrix r a) where
   show m@(M (r, _) _) = intercalate "\n" lines where
     lines = map (show . row m) [1..r]
 
 instance Functor (Matrix Fn) where
-  fmap f (M dim rep) = M dim (Fn (f . (elem rep)))
+  fmap f (M dim (Fn g)) = M dim (Fn (f . g))
 
 instance Applicative (Matrix Fn) where
   pure x = M (0, 0) (Fn (\_ -> x))
@@ -70,26 +77,23 @@ multiply :: (Num a, MatrixRep r) => Matrix r a -> Matrix r a -> Matrix Fn a
 multiply m1@(M (r1, c1) _) m2@(M (r2, c2) _) =
   if c1 == r2 then result else exception where
     result = M (r1, c2) (Fn h)
-    h (i, j) = sum (List.zipWith (*) (row m1 i) (column m2 j))
+    h (i, j) = sum $ List.zipWith (*) (row m1 i) (column m2 j)
     exception = error "Matrix.multiply: matrices with incompatible dimensions"
 
 transpose :: MatrixRep r => Matrix r a -> Matrix Fn a
 transpose (M (r, c) rep) = M (c, r) (Fn f) where
   f (i, j) = elem rep (j, i)
 
--- update :: Matrix a -> RectIdx -> a -> Matrix a
--- update (M dim f) pos val = M dim g where
---   g pos' = if pos' == pos then val else f pos'
+update :: Matrix Arr a -> RectIdx -> a -> Matrix Arr a
+update (M dim (Arr arr)) pos val = M dim (Arr arr') where
+  arr' = runSTArray $ do
+    marr <- unsafeThaw arr
+    writeArray marr pos val
+    pure marr
 
--- updates :: Matrix a -> [(RectIdx, a)] -> Matrix a
--- updates m = foldl' f m where
---   f mtx (pos, val) = update mtx pos val
-
--- freeze :: forall a. Matrix a -> Matrix a
--- freeze (M dim@(r, c) f) = M dim g where
---   g pos = arr ! pos
---   arr :: Array RectIdx a
---   arr = listArray ((1, 1), dim) (map f ((,) <$> [1..r] <*> [1..c]))
+updates :: Matrix Arr a -> [(RectIdx, a)] -> Matrix Arr a
+updates m = foldl' f m where
+  f mtx (pos, val) = update mtx pos val
 
 fromList :: forall a. RectIdx -> [a] -> Matrix Arr a
 fromList dim xs = M dim (Arr arr) where
