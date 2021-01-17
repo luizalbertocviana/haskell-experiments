4d3f558076e627e05024701348546868825081a6
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2021-01-16 21:24:32 -0300

    changed Digraph module to Graph module

---
+++ Digraph.hs
@@ -0,0 +1,69 @@
+module Digraph () where
+
+import Data.Word ( Word64 )
+import Combinatorial (cartesian)
+
+import Prelude (Bool(..), (<), (&&), const, (+), (-), filter, ($), (||), not, (==), (/=), zip, repeat, otherwise, map, fst, snd)
+
+type Vertex = Word64
+type Arc = (Vertex, Vertex)
+
+data Digraph = D Word64 (Arc -> Bool)
+
+verify :: Digraph -> Arc -> Bool
+verify (D n _) (u, v) = u < n && v < n
+
+empty :: Word64 -> Digraph
+empty n = D n (const False)
+
+complete :: Word64 -> Digraph
+complete n = D n (const True)
+
+vertices :: Digraph -> [Vertex]
+vertices (D n _) = [0..n - 1]
+
+arcs :: Digraph -> [Arc]
+arcs d@(D _ adj) = filter adj $ cartesian verts verts where
+  verts = vertices d
+
+hasArc :: Digraph -> Arc -> Bool
+hasArc d@(D _ adj) arc = verify d arc && adj arc
+
+addArc :: Digraph -> Arc -> Digraph
+addArc d@(D n adj) arc =
+  if adj arc || not (verify d arc)
+  then d
+  else D n adj' where
+    adj' e = e == arc || adj e
+
+removeArc :: Digraph -> Arc -> Digraph
+removeArc d@(D n adj) arc =
+  if adj arc && verify d arc
+  then D n adj'
+  else d where
+    adj' e = e /= arc && adj e
+
+outArcs :: Digraph -> Vertex -> [Arc]
+outArcs d@(D n adj) u
+  | u < n = filter adj $ zip (repeat u) (vertices d)
+  | otherwise = []
+
+inArcs :: Digraph -> Vertex -> [Arc]
+inArcs d@(D n adj) v
+  | v < n = filter adj $ zip (vertices d) (repeat v)
+  | otherwise = []
+
+outNeighbors :: Digraph -> Vertex -> [Vertex]
+outNeighbors d u = map snd (outArcs d u)
+
+inNeighbors :: Digraph -> Vertex -> [Vertex]
+inNeighbors d v = map fst (inArcs d v)
+
+path :: Word64 -> Digraph
+path n = D n adj where
+  adj (u, v) = v == u + 1
+
+cycle :: Word64 -> Digraph
+cycle n = D n adj where
+  adj arc = arc == (n - 1, 0) || adjPath arc
+  (D _ adjPath) = path n
